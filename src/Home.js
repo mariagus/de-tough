2a2fb984027b98b6558c115f8dc4be5d1dbdbc16
Maria Gusova <masha.gu@gmail.com>
2021-01-10 22:49:23 +0000

    adjust slogan

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,8 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="home">
-      <p class="welcome">Your complete property solutions</p>
+      <br />
+      <p class="welcome">Your Complete Property Solutions</p>
     </div>
   );
 }
